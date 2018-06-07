7dcc8c25102b3a2463caa6ea348290acd394f5fd
Karen <hfgc111@gmail.com>
2018-06-07 23:45:32 +0800

    Initial Commit

---
+++ letsPicnic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  letsPicnic
+//
+//  Created by gary chen on 2018/6/7.
+//  Copyright © 2018 gary chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
